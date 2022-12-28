dd6d1f0bd43d0d11ed55ecf4917e5caa767660e5
odetacocaj <54553185+odetacocaj@users.noreply.github.com>
2022-12-28 15:12:07 +0100

    Cart Controller

--- BulkyBook.DataAccess/Migrations/20221227222225_addOrderHeaderAndOrderDetail.Designer.cs
+++ BulkyBook.DataAccess/Migrations/20221227222225_addOrderHeaderAndOrderDetail.Designer.cs
@@ -4,6 +4,7 @@ using BulkyBook.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyBookWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221227222225_addOrderHeaderAndOrderDetail")]
+    partial class addOrderHeaderAndOrderDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -173,7 +175,7 @@ namespace BulkyBookWeb.Migrations
 
                     b.HasIndex("ApplicationUserId");
 
-                    b.ToTable("OrderHeaders");
+                    b.ToTable("OrderHeader");
                 });
 
             modelBuilder.Entity("BulkyBook.Models.Product", b =>
--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -15,6 +15,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 
         IShoppingCartRepository ShoppingCart { get; }
         IApplicationUserRepository ApplicationUser { get; }
+        IOrderDetailRepository OrderDetail { get; }
+        IOrderHeaderRepository OrderHeader { get; }
         void Save();
     }
 }
